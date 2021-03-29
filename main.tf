a24bcd00480d9db9c1902212c738ed457329313c
Denyse <dhuezo@gmail.com>
2021-03-29 13:44:41 -0400

    init

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "example" {
+  ami                    =  "ami-0533f2ba8a1995cf9"
+  instance_type          = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "Denyse-ec2-example"
+  }
+}
